e2773776baa44a921a5056637a25c7a62f5a0753
Camila Escalante <escalantecamila.n@gmail.com>
2025-08-23 17:26:35 -0300

    standalone - pages

--- src/app/pages/guards/events/view-events/view-events.page.ts
+++ src/app/pages/guards/events/view-events/view-events.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { ModalController } from '@ionic/angular';
+//Componenetes
+import { NavbarBackComponent } from 'src/app/components/navbars/navbar-back/navbar-back.component';
 import { InvitationsComponent } from 'src/app/components/invitations/invitations/invitations.component';
+//Interfaces y Servicios
 import { AmenitieInterface } from 'src/app/interfaces/amenitie-interface';
 import { ReservationsInterface } from 'src/app/interfaces/reservations-interface';
 import { ReservationsService } from 'src/app/services/amenities/reservations.service';
@@ -10,12 +15,16 @@ import { CountryStorageService } from 'src/app/services/storage/country-storage.
   selector: 'app-view-events',
   templateUrl: './view-events.page.html',
   styleUrls: ['./view-events.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    NavbarBackComponent
+  ]
 })
 
 export class ViewEventsPage implements OnInit {
-
   protected reservations: any[]
-
   constructor(private modalCtrl: ModalController,
               private reservationService: ReservationsService,
               private _countryStorage: CountryStorageService) { }
@@ -23,26 +32,21 @@ export class ViewEventsPage implements OnInit {
   async ngOnInit() {
 
     const id_country = await (await this._countryStorage.getCountry()).id
-
     this.reservationService.getAllByCountryAndStatus(id_country, "aprobado").subscribe(
       reservations => {
         this.reservations = reservations
       }
     )
-
   }
-
-  
 
   async openModal(reservation, index) {
     console.log(reservation, index);
+    
     const id_reservation = reservation.id
-
     this.reservationService.reservationGuests(id_reservation).subscribe(
       async guests => {
 
         console.log(guests);
-
         const modal = await this.modalCtrl.create({
           component: InvitationsComponent,
           mode: 'ios',
@@ -53,14 +57,7 @@ export class ViewEventsPage implements OnInit {
         modal.present();
     
         const { data, role } = await modal.onWillDismiss();
-
-      } 
+      }
     )
-    
   }
-
-  
-
-
-
 }
--- src/app/pages/guardsSchedule/guards-schedule/guards-schedule.page.ts
+++ src/app/pages/guardsSchedule/guards-schedule/guards-schedule.page.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import * as moment from 'moment';
 import { GuardsService } from 'src/app/services/guards/guards.service';
+import { NavbarBackComponent } from 'src/app/components/navbars/navbar-back/navbar-back.component';
 
 @Component({
   selector: 'app-guards-schedule',
   templateUrl: './guards-schedule.page.html',
   styleUrls: ['./guards-schedule.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    NavbarBackComponent
+  ]
 })
 export class GuardsSchedulePage implements OnInit {
 
   guards: any[]
-
-
 
   constructor(private _guardsService: GuardsService) { }
 
