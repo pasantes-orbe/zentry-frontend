9eb6871afb380ec5673c18874c17ef8a8ce45981
Camila Escalante <escalantecamila.n@gmail.com>
2025-08-24 00:48:32 -0300

    country-concurrents.pages.ts

--- src/app/pages/admin/country-recurrents/country-recurrents.page.ts
+++ src/app/pages/admin/country-recurrents/country-recurrents.page.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SearchbarCustomEvent } from '@ionic/angular';
 
-// Servicios y otros
-import { RecurrentsInterface } from 'src/app/interfaces/recurrents-interface';
+//Servicios
+import { AlertService } from 'src/app/services/helpers/alert.service';
+import { PropertiesService } from '../../../services/properties/properties.service';
 import { RecurrentsService } from 'src/app/services/recurrents/recurrents.service';
 import { OwnerStorageService } from 'src/app/services/storage/owner-interface-storage.service';
 
+//Interfaces
+import { RecurrentsInterface } from 'src/app/interfaces/recurrents-interface';
+
+//Componentes
+import { NavbarBackComponent } from "src/app/components/navbars/navbar-back/navbar-back.component";
+
 @Component({
   selector: 'app-recurrents-view-all',
-  templateUrl: './add-recurrentrecurrents-view-all/recurrents-view-all.component.html',
-  styleUrls: ['./recurrents-view-all.component.scss'],
+  templateUrl: './country-recurrents.page.html',
+  styleUrls: ['./country-recurrents.page.scss'],
   standalone: true,
   imports: [
     CommonModule, // Para *ngIf y *ngFor
-    FormsModule,  // Para [(ngModel)] en la barra de búsqueda
-    IonList,
-    IonItem,
-    IonLabel,
-    IonToggle,
-    IonSearchbar
+    IonicModule,
+    ReactiveFormsModule,
+    NavbarBackComponent
   ]
 })
 export class RecurrentsViewAllComponent implements OnInit {
